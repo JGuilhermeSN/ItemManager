dcc0d8b31d7d6bb53d94884a5f1b35d420674120
JGuilhermeSN <joseguilhermesn.jg@gmail.com>
2024-04-29 18:24:08 -0300

    test commit

--- lib/components/app_colors.dart
+++ lib/components/app_colors.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static final appbar = Color(0xFF2F4B4C);
+  static final lighterColor = Color.fromARGB(255, 85, 181, 184);
   static final darkerColor = Color(0xFF190A30);
   static final mainColor = Color(0xFF453745);
   static final redColor = Color(0xFFe71837);
